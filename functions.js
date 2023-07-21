2cee26cfc1e132ced3fb00a050af3228b84539e5
mathewCodex <mathewemmanuel212@gmail.com>
2023-07-21 19:26:55 +0100

    updated and added some cool UI 😎

--- functions.js
+++ functions.js
@@ -1,15 +1,49 @@
 "use strict";
 
+const getYear = document.getElementById("getYear");
 const newDate = new Date();
 const getyear = newDate.getFullYear();
 console.log(getyear);
-
-const text1 = ["my name is mathew Emmanuel,"];
-
+getYear.textContent = getyear;
+const text1 = ["my name is Mathew Emmanuel,"];
+const local = document.getElementById("getDate");
+const local1 = document.getElementById("getDate1");
+const local2 = document.getElementById("getDate2");
+const local3 = document.getElementById("getDate3");
 let count = -0;
 let index = 0;
 let currentText = "";
 let letter = " ";
+let opts = {
+  day: "numeric",
+  year: "numeric",
+  month: "short",
+};
+let localDate = Intl.DateTimeFormat("en-US", opts).format(newDate);
+local.textContent = localDate;
+local1.textContent = localDate;
+local2.textContent = localDate;
+local3.textContent = localDate;
+
+// Hamburger jquery
+$(".hamburger").on("click", function (e) {
+  e.preventDefault();
+  $(this).toggleClass("navicon--active");
+  $(".toggle").toggleClass("toggle--active");
+});
+///
+// // Find all sections with the "date-section" class
+// const dateSections = document.getElementById('getDate');
+
+// // Loop through each section and display the date
+// for (const section of dateSections) {
+//     // Create a new paragraph element to display the date
+//     const paragraph = document.createElement('h5');
+//     paragraph.textContent = ${dateString};
+
+//     // Append the paragraph element to the section
+//     section.appendChild(paragraph);
+// }
 
 // (function type() {
 //   if (count === texts.length) {
@@ -33,7 +67,7 @@ const wait = function (seconds) {
 };
 //multple
 wait(1).then(() => {
-  console.log("1 second Passed");
+  // console.log("1 second Passed");
   (function type() {
     if (count === text1.length) {
       count = 30;
@@ -51,10 +85,10 @@ wait(1).then(() => {
   return wait(1);
 });
 wait(5).then(() => {
-  console.log("2 second Passed");
+  // console.log("2 second Passed");
   (function type() {
     const textopacity = document.querySelector(".typing1");
-    console.log(textopacity);
+    // console.log(textopacity);
     textopacity.style.opacity = 1;
     //Passing "arguments" into handler...
   })();
@@ -78,21 +112,7 @@ wait(15).then(() => {
 });
 
 // Reveal sections making the opacity 1;
-const allSections = document.querySelector(".my-Sec");
 
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
 function myFunction(imgs) {
   // Get the expanded image
   var expandImg = document.getElementById("expandedImg");
@@ -113,20 +133,6 @@ function myFunction(imgs) {
   // Show the container element (hidden with CSS)
 }
 
-// function myFunction(x) {
-//   if (x.matches) {
-//     // If media query matches
-//     var bigscreen = document.querySelector(".container");
-//     bigscreen.style.display = "block ";
-//     alert("matches");
-//   } else {
-//     var expandImg = document.getElementById("expandedImg");
-//     expandImg.style.display = "block";
-//     bigscreen.style.display = "block";
-//     alert("blocks");
-//   }
-// }
-
 // myFunction(x); // Call listener function at run time
 //x.addListener(myFunction) // Attach listener function on state changes
 const randomInt = (min, max) =>
@@ -143,48 +149,72 @@ document.querySelector(".colors").addEventListener("click", function (e) {
 
 //////text color
 
-// // PART 1
-// const loadNPause = async function () {
-//   try {
-//     // Load image 1
-//     let img = await createImage("img/img-1.jpg");
-//     console.log("Image 1 loaded");
-//     await wait(2);
-//     img.style.display = "none";
-
-//     // Load image 1
-//     img = await createImage("img/img-2.jpg");
-//     console.log("Image 2 loaded");
-//     await wait(2);
-//     img.style.display = "none";
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-/////////////////////////////////////////////////Portfolio page javascript..
-
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-
-//add fixed class to navbar
-
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
-  } else {
-    navbar.classList.remove("navbar-fixed");
-    console.log("ok");
-  }
+// swiper
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
 });
+// hamburger js
+var paperMenu = {
+  $window: $("#paper-window"),
+  $paperFront: $("#paper-front"),
+  $hamburger: $(".hamburger"),
+  offset: 1800,
+  pageHeight: $("#paper-front").outerHeight(),
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  console.log("ok1");
-}
+  open: function () {
+    this.$window.addClass("tilt");
+    this.$hamburger.off("click");
+    $("#container, .hamburger").on("click", this.close.bind(this));
+    this.hamburgerFix(true);
+    console.log("opening...");
+  },
+  close: function () {
+    this.$window.removeClass("tilt");
+    $("#container, .hamburger").off("click");
+    this.$hamburger.on("click", this.open.bind(this));
+    this.hamburgerFix(false);
+    console.log("closing...");
+  },
+  updateTransformOrigin: function () {
+    scrollTop = this.$window.scrollTop();
+    equation = ((scrollTop + this.offset) / this.pageHeight) * 100;
+    this.$paperFront.css("transform-origin", "center " + equation + "%");
+  },
+  //hamburger icon fix to keep its position
+  hamburgerFix: function (opening) {
+    if (opening) {
+      $(".hamburger").css({
+        position: "absolute",
+        top: this.$window.scrollTop() + 30 + "px",
+      });
+    } else {
+      setTimeout(function () {
+        $(".hamburger").css({
+          position: "fixed",
+          top: "30px",
+        });
+      }, 300);
+    }
+  },
+  bindEvents: function () {
+    this.$hamburger.on("click", this.open.bind(this));
+    $(".close").on("click", this.close.bind(this));
+    this.$window.on("scroll", this.updateTransformOrigin.bind(this));
+  },
+  init: function () {
+    this.bindEvents();
+    this.updateTransformOrigin();
+  },
+};
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  console.log("ok");
-}
+paperMenu.init();
